65239873b73454e85588baea6a750a5986e8cd61
San <834026555@qq.com>
2019-06-18 11:44:47 +0800

    add base html

--- fire/login/views.py
+++ fire/login/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.shortcuts import render,redirect
+
+def warning(request):
+    pass
+    return render(request, 'base.html')
