647bb36251759c823f65d09b1269b212f08e003a
San <834026555@qq.com>
2019-06-23 18:53:02 +0800

    fix the click pic problems

--- fire/static/js/warning.js
+++ fire/static/js/warning.js
@@ -1,27 +1,29 @@
-let img =
-    document.getElementById('warning-table').getElementsByTagName('img');
-let len = img.length;
-let popup = document.getElementById('popup');
+function rebind(){
+    let img =
+        document.getElementById('warning-table').getElementsByTagName('img');
+    let len = img.length;
+    let popup = document.getElementById('popup');
 
 
-for(let i=0;i<len ;i++){
+    for(let i=0;i<len ;i++){
 
-    img[i].onclick = ((event)=>{
-        // alert(12333);
-        // event = event|window.event;
-        // alert(event);
-        // let target = document.elementsFromPoint(event.clientX, event.clientY);
-        showBig(img[i].src);
-    })
-}
+        img[i].onclick = ((event)=>{
+            // alert(12333);
+            // event = event|window.event;
+            // alert(event);
+            // let target = document.elementsFromPoint(event.clientX, event.clientY);
+            showBig(img[i].src);
+        })
+    }
 
 
-popup.onclick = function () {
-    popup.style.display = "none";
-};
+    popup.onclick = function () {
+        popup.style.display = "none";
+    };
 
-function showBig(src) {
-    popup.getElementsByTagName('img')[0].src = src;
-    popup.style.display = "block";
+    function showBig(src) {
+        popup.getElementsByTagName('img')[0].src = src;
+        popup.style.display = "block";
+    }
 }
 
