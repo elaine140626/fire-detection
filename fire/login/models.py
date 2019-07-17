6578d3869f80b87f58bed7c71706e6e5bb584f01
plutozhang <834026555@qq.com>
2019-07-17 23:32:37 +0800

    添加菜单点击相应测试部分

--- fire/login/models.py
+++ fire/login/models.py
@@ -1,5 +1,7 @@
 # login/models.py
 from django.db import models
+
+
 class User(models.Model):
     '''用户表'''
     id = models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')
@@ -9,8 +11,11 @@ class User(models.Model):
     c_time = models.DateTimeField(auto_now_add=True, verbose_name="注册时间")
     phone = models.CharField(max_length=11, verbose_name="手机号")
     layout = models.IntegerField(verbose_name='布局设置', default=4)
+    wechat_id = models.CharField(max_length=128, verbose_name="微信openID", default="")
+
     def __str__(self):
         return self.name
+
     class Meta:
         ordering = ['c_time']
         verbose_name = '用户'
@@ -22,6 +27,7 @@ class Video(models.Model):
     id = models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')
     user_id = models.ForeignKey(to='User', to_field='id', on_delete=models.CASCADE, verbose_name='用户名')
     video_url = models.CharField(max_length=256, verbose_name='视频地址')
+
 
 class Device(models.Model):
     '设备'
@@ -36,10 +42,11 @@ class Device(models.Model):
 class Message(models.Model):
     '消息流'
     id = models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')
-    content = models.CharField(max_length=256,verbose_name='告警信息')
+    content = models.CharField(max_length=256, verbose_name='告警信息')
     img_url = models.CharField(max_length=256, verbose_name='图片链接')
     c_time = models.DateTimeField(auto_now_add=True, verbose_name='时间')
-    serial_number = models.ForeignKey(to='Device', to_field='serial_number', verbose_name='设备号', on_delete=models.CASCADE)
+    serial_number = models.ForeignKey(to='Device', to_field='serial_number', verbose_name='设备号',
+                                      on_delete=models.CASCADE)
 
 
 class User_Message(models.Model):
@@ -47,5 +54,3 @@ class User_Message(models.Model):
     user_id = models.ForeignKey(to='User', to_field='id', on_delete=models.CASCADE, verbose_name='用户名')
     message_id = models.ForeignKey(to='Message', to_field='id', on_delete=models.CASCADE, verbose_name='信息id')
     true_or_false = models.BooleanField(verbose_name='预警正误')
-
-
--- fire/login/reply.py
+++ fire/login/reply.py
@@ -47,8 +47,17 @@ class ImageMsg(Msg):
         return XmlForm.format(**self.__dict)
 
 
-def DealWithEventMsg(recMsg : receive.EventMsg):
-    if recMsg.EventKey == "BIND_ACCOUNT":
-        return TextMsg(recMsg.FromUserName, recMsg.ToUserName, "输入\"bind 用户名 密码\"来绑定账户\n如用户名为： 123 密码为：456\n那么输入\"bind 123 456\"")
+def DealWithEventMsg(rec_msg: receive.EventMsg):
+    if rec_msg.EventKey == "BIND_ACCOUNT":
+        return TextMsg(rec_msg.FromUserName, rec_msg.ToUserName,
+                       "输入\"bind 用户名 密码\"来绑定账户\n如用户名为： 123 密码为：456\n那么输入\"bind 123 456\"(不带引号)")
     else:
-        return TextMsg(recMsg.FromUserName, recMsg.ToUserName, "这个功能还莫得开发")
+        return TextMsg(rec_msg.FromUserName, rec_msg.ToUserName, "这个功能还莫得开发")
+
+
+def DealWithTextMsg(rec_msg: receive.TextMsg):
+    content = rec_msg.Content
+    if content[:4] == "bind":
+        return TextMsg(rec_msg.FromUserName, rec_msg.ToUserName, "bind")
+    else:
+        return TextMsg(rec_msg.FromUserName, rec_msg.ToUserName, "not bind")
