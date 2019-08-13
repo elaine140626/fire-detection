27b9de382048c529418baa2d6852906f050b1a83
plutozhang <834026555@qq.com>
2019-08-13 10:27:47 +0800

    fix the midlleware bug

--- fire/login/middleware.py
+++ fire/login/middleware.py
@@ -1,17 +1,26 @@
 from django.shortcuts import redirect
 from django.utils.deprecation import MiddlewareMixin
+from django.http import HttpResponse
+import login.status_code as code
 import json
 
-class LoginFilter(MiddlewareMixin):
 
-    white_list = ['/login/']
+class LoginFilter(MiddlewareMixin):
+    api_prefix = '/api/'
+    login_api = 'api/login/'
+
     def process_request(self, request):
         next_url = request.path_info
-        if next_url in self.white_list or request.session.get('is_login') :
-            return
+        if next_url[0: len(self.api_prefix) - 1] == self.api_prefix:
+            if next_url[0: len(self.login_api) - 1] == self.login_api:
+                return
+            if request.session.get('is_login'):
+                return
+            else:
+                ret = {
+                    "code": code.LOGIN_FILTER_NOT_LOGIN,
+                    "msg": "not login"
+                }
+                return HttpResponse(json.dumps(ret))
         else:
-            ret = {
-                "code": 10001,
-                "msg": "not login"
-            }
-            return redirect('/login/')
+            return
