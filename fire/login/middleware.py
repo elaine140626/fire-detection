4d538f230fec9237000624a2fda7be7983eb20ca
plutozhang <834026555@qq.com>
2019-08-14 20:56:51 +0800

    fix the pattern problem and test the message method

--- fire/login/middleware.py
+++ fire/login/middleware.py
@@ -7,12 +7,15 @@ import json
 
 class LoginFilter(MiddlewareMixin):
     api_prefix = '/api/'
-    login_api = 'api/login/'
+    login_api = '/api/user/login/'
 
     def process_request(self, request):
         next_url = request.path_info
-        if next_url[0: len(self.api_prefix) - 1] == self.api_prefix:
-            if next_url[0: len(self.login_api) - 1] == self.login_api:
+        print(next_url[0: len(self.api_prefix)])
+        if next_url[0: len(self.api_prefix)] == self.api_prefix:
+            print(next_url[0: len(self.login_api)])
+            print(next_url)
+            if next_url[0: len(self.login_api)] == self.login_api:
                 return
             if request.session.get('is_login'):
                 return
