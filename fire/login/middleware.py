04d3b17de8fc8fd62b399bc62860b16b5d21cb4b
plutozhang <834026555@qq.com>
2019-08-19 17:04:29 +0800

    fix filter bug

--- fire/login/middleware.py
+++ fire/login/middleware.py
@@ -1,6 +1,7 @@
 from django.shortcuts import redirect
 from django.utils.deprecation import MiddlewareMixin
 from django.http import HttpResponse
+from django.core.handlers import wsgi
 import login.status_code as code
 import json
 
@@ -9,10 +10,11 @@ class LoginFilter(MiddlewareMixin):
     api_prefix = '/api/'
     login_api = '/api/user/login/'
 
-    def process_request(self, request):
+    def process_request(self, request: wsgi.WSGIRequest):
         next_url = request.path_info
         print(next_url[0: len(self.api_prefix)])
-        if next_url[0: len(self.api_prefix)] == self.api_prefix:
+        print(request.get_host())
+        if next_url[0: len(self.api_prefix)] == self.api_prefix and request.get_host() != "127.0.0.1:8000":
             print(next_url[0: len(self.login_api)])
             print(next_url)
             if next_url[0: len(self.login_api)] == self.login_api:
